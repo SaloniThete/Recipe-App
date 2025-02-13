aee3baf1f7aea128be6788248e8235d3d8856ab0
Saloni_Thete <Saloni662001@gmail.com>
2025-02-13 14:25:08 +0530

    Added recipe splash screen

---
+++ src/screens/DrawerContainer/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    marginVertical:'30%',
+    flexDirection: 'row',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    paddingHorizontal: 20,
+  }
+});
+
+export default styles;
